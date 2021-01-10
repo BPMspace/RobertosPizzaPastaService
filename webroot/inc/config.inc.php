d9d18198a3c89dae1c2708e4c59d5bf1ffbb98bc
root <root@vmd57631.contaboserver.net>
2021-01-10 09:37:28 +0100

    menu working incl img

--- webroot/inc/config.inc.php
+++ webroot/inc/config.inc.php
@@ -3,7 +3,8 @@
 	$TEAM_NUMBER = getenv('Number_Pizza_Team');
 	$_SESSION['REMOTE_USER'] = ((isset($_SERVER['REMOTE_USER'])) ? $_SERVER['REMOTE_USER'] : 'Guest') ;
 	$USER = $_SESSION['REMOTE_USER'];
-   
+	$PHP_SELF = $_SERVER['PHP_SELF'];
+	$INTERNAL = ((strpos($PHP_SELF, 'internal') == false) ? false : true);
   
    $TEAM_NUMBER_BUTTON =
 			"<div class=\"btn team".$TEAM_NUMBER." btn-lg\">TEAM: ".$TEAM_NUMBER." Round: ". $SIMULATION_ROUND. " User: ".$USER."</div>";
---
+++ webroot/inc/menu.sub.extra.inc.php
@@ -0,0 +1,45 @@
+      <div class="row justify-content-center mb-5 pb-3 mt-5 pt-5">
+         <div class="col-md-7 heading-section text-center ftco-animate">
+            <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
+            <div class="top-buffer_60">
+               <h2 class="mb-4"><br/>Extra</h2>
+            </div>
+         </div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="row">
+            <?php
+            // Create connection
+            $dsn = 'mysql:dbname='.$dbname.';host='.$servername.';port=3306;charset=utf8';
+            $conn = new PDO($dsn, $username, $dbpassword);
+
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+
+            try {
+            $query = $conn->prepare("SELECT * FROM `MENU` WHERE `type` = \"Extra\" ");
+            $query->execute();
+            }
+            catch (PDOException $e) {
+              $error=$e->getMessage();
+            }
+            while ($row = $query->fetch(PDO::FETCH_ASSOC))
+            {
+                echo '<div class="col-md-6">';
+                echo '<div class="pricing-entry d-flex ftco-animate">';
+                echo '<div class="img" style="background-image: url('.$row['img'].');"></div>';
+                echo '<div class="desc pl-3">';
+                echo '<div class="d-flex text align-items-center">';
+                echo '<h3><span>'.$row['name'].'</span></h3>';
+                echo '<span class="price">'.$row['price'].'</span>';
+                echo '</div><div class="d-block"><p>'.$row['ingredients'].'</p></div>';
+                echo '</div></div></div>';
+            } ?>
+            </div>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
