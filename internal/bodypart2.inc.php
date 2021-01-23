a6f04a29805f19672499938257d1fbd3aad06314
root <root@vmd57631.contaboserver.net>
2021-01-23 09:21:04 +0100

    new imag size

--- internal/bodypart2.inc.php
+++ internal/bodypart2.inc.php
@@ -1,4 +1,8 @@
 <div class="col-md-12">
+   <div class="top-buffer_60 heading-section text-center ftco-animate">
+		<p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
+        <h2 class="mb-4"><br/><?php echo $ROOM;?></h2>
+   </div>
    <div id="meeting" class="fixed_meeting"></div>
 </div>
 <div class="col-md-12 top-buffer_60">
